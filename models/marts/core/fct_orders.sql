36be4f1c670909445a685f44bbe43ffe7181e423
remek.kosiorek@gmail.com <remek.kosiorek@gmail.com>
2021-12-05 23:19:34 +0000

    Chapter: Analyses and Seeds- Implementing seeds

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -5,6 +5,9 @@ with orders as  (
 payments as (
     select * from {{ ref('stg_payments') }}
 ),
+employees as (
+    select * from {{ ref('employees') }}
+),
 
 order_payments as (
     select
@@ -21,10 +24,12 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
+        employees.employee_id is not null as is_employees,
         coalesce(order_payments.amount, 0) as amount
 
     from orders
     left join order_payments using (order_id)
+    left join employees using (customer_id)
 )
 
 select * from final
